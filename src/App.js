1e5151a8b8b7a9fd631dade927613bca78f59e94
Catalin <catalin.sandru88@gmail.com>
2020-12-03 16:41:27 +0200

    made a scrollable component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css'
 
 
@@ -9,9 +9,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots,
+      robots: [],
       searchField: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(users => {
+        this.setState({robots: users})
+      })
   }
 
   onSearchChange = (event) => {
@@ -27,7 +37,9 @@ class App extends Component {
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
+        <Scroll>
+          <CardList robots={filteredRobots}/>
+        </Scroll>
       </div>
     )
   }
